b409f55e9aeaa5de8aad1f484b6dd7d74b7c9170
Alex Widener <awidener3@gmail.com>
2022-02-10 11:06:56 -0500

    fixed typo /api/user

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const collectionRoutes = require('./collectionRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/collection', collectionRoutes);
 
 module.exports = router;

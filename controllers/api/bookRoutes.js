31906d8207a8f23e28b5dbd79bfad23c714fe826
ML <labrecquemegan@hotmail.com>
2022-02-10 18:25:50 -0500

    added get book by ID function in the book routes js, have not tested it yet

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -43,6 +43,24 @@ router.get('/library', async (req, res) => {
 });
 
 // TODO: Get book by id
+router.get('/:id', async (req, res) => {
+	try {
+	  const bookData = await Book.getById({
+		where: {
+		  id: req.params.id,
+		},
+	  });
+  
+	  if (!bookData) {
+		res.status(404).json({ message: 'No book found with this id!' });
+		return;
+	  }
+  
+	  res.status(200).json(bookData);
+	} catch (err) {
+	  res.status(500).json(err);
+	}
+  });
 
 // Add book to collection
 router.post('/', withAuth, async (req, res) => {

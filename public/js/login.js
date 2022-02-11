d7f2709549a90d30feb055346b25890b8bafa70e
ML <labrecquemegan@hotmail.com>
2022-02-10 20:43:56 -0500

    restyled signin/up and redid some logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,54 @@
+// login logic
 const loginFormHandler = async (event) => {
-	event.preventDefault();
+  event.preventDefault();
 
-	const email = document.querySelector('#email-login').value.trim();
-	const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-	if (email && password) {
-		const response = await fetch('/api/users/login', {
-			method: 'POST',
-			body: JSON.stringify({ email, password }),
-			headers: { 'Content-Type': 'application/json' },
-		});
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-		if (response.ok) {
-			document.location.replace('/home');
-		} else {
-			alert('Failed to log in');
-		}
-	}
+    if (response.ok) {
+      document.location.replace("/home");
+    } else {
+      alert("Failed to log in");
+    }
+  }
 };
 
 document
-	.querySelector('.login-form')
-	.addEventListener('submit', loginFormHandler);
+  .querySelector(".sign-in-htm")
+  .addEventListener("submit", loginFormHandler);
+
+
+
+// signup logic
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/home");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".sign-up-htm")
+  .addEventListener("submit", signupFormHandler);

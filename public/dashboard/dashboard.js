b59493c0eebecfc1a4cbe21457169e32068050b4
xndrbrgs <aborgescolon@gmail.com>
2022-02-11 12:59:15 -0500

    main dashboard v1

---
+++ public/dashboard/dashboard.js
@@ -0,0 +1,40 @@
+ScrollReveal().reveal('.nav_links', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+  
+  ScrollReveal().reveal('#content', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+
+  
+  ScrollReveal().reveal('.logout', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+  
+  ScrollReveal().reveal('.list-group', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400,
+    origin: 'left'
+  });
+  
+  ScrollReveal().reveal('.books-list', {
+    reset: false,
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    origin: 'right'
+  });
+
+
+  

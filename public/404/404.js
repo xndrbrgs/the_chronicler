dc504b2634337b1abd236bb02dc10260ad5abb09
xndrbrgs <aborgescolon@gmail.com>
2022-02-11 14:00:03 -0500

    404 route

---
+++ public/404/404.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.nav_links', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+  
+  ScrollReveal().reveal('#content', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+
+  
+  ScrollReveal().reveal('.logout', {
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  });
+  
+  ScrollReveal().reveal('.main-page', {
+    reset: false,
+    distance: '60px',
+    duration: 4000,
+    delay: 400,
+    origin: 'bottom'
+  });

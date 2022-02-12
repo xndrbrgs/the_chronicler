07a384484a256d86b1efa98ec3268a8336a85ca2
Alex Widener <awidener3@gmail.com>
2022-02-12 11:46:49 -0500

    fixed messy comments

--- models/index.js
+++ models/index.js
@@ -2,9 +2,6 @@ const User = require('./User');
 const Book = require('./Book');
 const UserBook = require('./UserBook');
 
-// User.belongsToMany(Book, {through: 'User-Books'});
-// Book.belongsToMany(User, {through: 'User-Books'});
-
 User.belongsToMany(Book, {
   through: {
     model: UserBook,
--- seeds/seed.js
+++ seeds/seed.js
@@ -4,46 +4,25 @@ const {User, Book, UserBook} = require('../models');
 // Bring in data
 const userData = require('./userData.json');
 const bookData = require('./bookData.json');
-// const userBookData = require('./userBookData.json');
 
 // Bulk create values in table
 const seedDatabase = async () => {
   await sequelize.sync({force: true});
 
-  // seeded users
-  //   const users = await User.bulkCreate(userData, {
-  //     individualHooks: true,
-  //     returning: true,
-  //   });
-
-  //   const books = await Book.bulkCreate(bookData, {
-  //     individualHooks: true,
-  //     returning: true,
-  //   });
-
+  //   seed user and books from json
   const users = await User.bulkCreate(userData);
-
   const books = await Book.bulkCreate(bookData);
 
-  //   seeded user/book associations
-  //   const userBooks = await UserBook.bulkCreate(userBookData, {
-  //     individualHooks: true,
-  //     returning: true,
-  //   });
-
+  //   create random associations for user-books
   for (let i = 0; i < 10; i++) {
-    // Get a random users id
+    // Get a random ids
     const {id: randomUserId} = users[Math.floor(Math.random() * users.length)];
-
-    // Get a random books id
     const {id: randomBookId} = books[Math.floor(Math.random() * books.length)];
 
-    // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
     await UserBook.create({
       user_id: randomUserId,
       book_id: randomBookId,
     }).catch((err) => {
-      // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
       console.log(err);
     });
   }

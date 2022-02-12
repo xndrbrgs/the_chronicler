455f028eb9c0aa5f1ed55545f4d8520f4496a324
Alex Widener <awidener3@gmail.com>
2022-02-12 11:13:50 -0500

    added seeding of book data through npm run seed

--- models/index.js
+++ models/index.js
@@ -2,7 +2,21 @@ const User = require('./User');
 const Book = require('./Book');
 const UserBook = require('./UserBook');
 
-User.belongsToMany(Book, {through: 'User-Books'});
-Book.belongsToMany(User, {through: 'User-Books'});
+// User.belongsToMany(Book, {through: 'User-Books'});
+// Book.belongsToMany(User, {through: 'User-Books'});
+
+User.belongsToMany(Book, {
+  through: {
+    model: UserBook,
+    unique: false,
+  },
+});
+
+Book.belongsToMany(User, {
+  through: {
+    model: UserBook,
+    unique: false,
+  },
+});
 
 module.exports = {User, Book, UserBook};
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,54 @@
 const sequelize = require('../config/connection');
-const { User, Book } = require('../models');
+const {User, Book, UserBook} = require('../models');
 
 // Bring in data
 const userData = require('./userData.json');
 const bookData = require('./bookData.json');
+// const userBookData = require('./userBookData.json');
 
 // Bulk create values in table
 const seedDatabase = async () => {
-	await sequelize.sync({ force: true });
+  await sequelize.sync({force: true});
 
-	const users = await User.bulkCreate(userData, {
-		individualHooks: true,
-		returning: true,
-	});
+  // seeded users
+  //   const users = await User.bulkCreate(userData, {
+  //     individualHooks: true,
+  //     returning: true,
+  //   });
 
-	process.exit(0);
+  //   const books = await Book.bulkCreate(bookData, {
+  //     individualHooks: true,
+  //     returning: true,
+  //   });
+
+  const users = await User.bulkCreate(userData);
+
+  const books = await Book.bulkCreate(bookData);
+
+  //   seeded user/book associations
+  //   const userBooks = await UserBook.bulkCreate(userBookData, {
+  //     individualHooks: true,
+  //     returning: true,
+  //   });
+
+  for (let i = 0; i < 10; i++) {
+    // Get a random users id
+    const {id: randomUserId} = users[Math.floor(Math.random() * users.length)];
+
+    // Get a random books id
+    const {id: randomBookId} = books[Math.floor(Math.random() * books.length)];
+
+    // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
+    await UserBook.create({
+      user_id: randomUserId,
+      book_id: randomBookId,
+    }).catch((err) => {
+      // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
+      console.log(err);
+    });
+  }
+
+  process.exit(0);
 };
 
 seedDatabase();

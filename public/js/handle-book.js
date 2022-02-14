d98e0455521f22607572a182abfd4339c5a40621
Alex Widener <awidener3@gmail.com>
2022-02-14 11:18:01 -0500

    handlebars fix

--- public/js/handle-book.js
+++ public/js/handle-book.js
@@ -1,12 +1,8 @@
-// when a plus button is pressed, get the data of that book and add it to the user
 const addBtn = document.querySelector('#add-btn');
 const deleteBtn = document.querySelector('#delete-btn');
 const bookId = document.querySelector('#details').dataset.id;
 
 const handleAddBook = async () => {
-  // check to see if the book already exists
-
-  // if not:
   const response = await fetch('/api/user/add', {
     method: 'POST',
     body: JSON.stringify({book_id: bookId}),
@@ -16,7 +12,6 @@ const handleAddBook = async () => {
   });
 
   if (response.ok) {
-    console.log('Your book has been added!');
     addBtn.classList.add('disabled');
     addBtn.textContent = 'Added';
   } else {
@@ -30,13 +25,11 @@ const handleDeleteBook = async () => {
   });
 
   if (response.ok) {
-    console.log('Your book has been deleted!');
     addBtn.classList.remove('disabled');
-    addBtn.textContent = 'Remove from Your Collection';
+    addBtn.textContent = 'Add to Your Collection';
     deleteBtn.style.display = 'none';
   } else {
     alert('Failed to delete book');
-    console.log(response);
   }
 };
 
